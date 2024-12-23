f4afd18773a79ad714eb67fd5687f25cdc347e74
Pranav Uikey <42734141+PranavUikey@users.noreply.github.com>
2024-12-23 17:39:20 +0530

    data_ingestion completed

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from end_2_end_ds_project import logger
+from end_2_end_ds_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info('Welcome to our customized logging!!!')
+
+STAGE_NAME = 'Data Ingestion stage'
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f'>>>>>>> stage {STAGE_NAME} started  <<<<<<<')
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<< \n\nx========x')
+    except Exception as e:
+        logger.exception(e)
+        raise e
