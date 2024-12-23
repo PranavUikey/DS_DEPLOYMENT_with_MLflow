bf4370cca906005b22164643dd711fea9bc69d89
Pranav Uikey <42734141+PranavUikey@users.noreply.github.com>
2024-12-23 15:51:44 +0530

    exceptions, utils and logger added

---
+++ main.py
@@ -0,0 +1,4 @@
+from end_2_end_ds_project import logger
+
+
+logger.info('Welcome to our customized logging!!!')
--- template.py
+++ template.py
@@ -30,8 +30,6 @@ list_of_files = [
     "research/trials.ipynb",
     "templates/index.html",
     "test.py"
-
-
 ]
 
 for filepath in list_of_files:
