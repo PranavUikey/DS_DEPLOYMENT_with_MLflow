94d71c644dd29a4afe51dd676dc5399335a31627
Pranav Uikey <42734141+PranavUikey@users.noreply.github.com>
2024-12-23 18:40:29 +0530

    data_validation done

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from end_2_end_ds_project import logger
 from end_2_end_ds_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+from end_2_end_ds_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
 
 
 STAGE_NAME = 'Data Ingestion stage'
 
 
-if __name__ == '__main__':
-    try:
-        logger.info(f'>>>>>>> stage {STAGE_NAME} started  <<<<<<<')
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<< \n\nx========x')
-    except Exception as e:
-        logger.exception(e)
-        raise e
+try:
+    logger.info(f'>>>>>>> stage {STAGE_NAME} started  <<<<<<<')
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<< \n\nx========x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = 'Data Validation stage'
+
+try:
+    logger.info(f'>>>>>>> stage {STAGE_NAME} started  <<<<<<<')
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<< \n\nx========x')
+except Exception as e:
+    logger.exception(e)
+    raise e
